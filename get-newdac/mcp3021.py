535d178d890f0ad991f566addfd379aea43d0f45
Vanin Klim <vanin.kv@phystech.edu>
2025-10-15 18:32:24 +0300

    edited  adc

--- get-newdac/mcp3021.py
+++ get-newdac/mcp3021.py
@@ -1,7 +1,7 @@
 import smbus
 import time
 
-dynamic_range = 5
+dynamic_range = 3.287
 
 class MCP3021:
     def __init__(self, dynamic_range, address=0x4d, verbose = True):
@@ -16,11 +16,8 @@ class MCP3021:
         self.dynamic_range = dynamic_range
 
     def read(self):
-        res = self.bus.read_word_data(self.address, 0)
-        msb = res & 0xFF
-        lsb = (res>>8) & 0xFF
-        numb = msb<<8 + lsb<<2 
-        return numb
+        res = self.bus.read_i2c_block_data(self.address, 0, 2)
+        return ((((res[0] & 0x0F) << 6)|(res[1]>>2))/650)*self.dynamic_range
 
     def deinit(self):
         self.bus.close()
